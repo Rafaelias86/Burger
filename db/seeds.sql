91c44650b34ad4a0783f69a672f69620cf2a7867
Rafa Elias <rafaeliasuzcategui@gmail.com>
2019-06-20 16:21:37 -0400

    Data-base and connection created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Burguer');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');

31403a9ce6459499fe50b6781f06009caf7f0de7
Rafa Elias <rafaeliasuzcategui@gmail.com>
2019-06-27 18:35:12 -0400

    Update files database, connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+require("dotenv").config();
+
+var connection;
+
+if (process.env.JAWSDB_URL){
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "17130179",
+  password: process.env.MYSQL_PASSWORD,
   database: "burgers_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
